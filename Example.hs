ef8a7c46778c0c16cdf5b62037251228a6156936
Alberto Centelles <centelles.alberto@gmail.com>
2018-05-04 22:25:54 +0100

    Separate Auth and Hash in different files

--- Example.hs
+++ Example.hs
@@ -1,16 +1,53 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveAnyClass #-}
+
 import Protolude hiding (Hashable)
+
 import Auth
+import Hash
 
-data Bit = L | R
-
-data Tree x
-  = Tip String
-  | Bin x x
+data AuthTree a
+  = AuthTip { tipHash :: Hash, tipValue :: a }
+  | AuthBin { binHash :: Hash, lAuthTree :: (AuthTree a), rAuthTree :: (AuthTree a) }
   deriving (Show, Functor, Generic)
 
-data List a
-  = Cons a (List a)
-  | Nil
-  deriving (Show, Functor, Generic)
+data Tree a
+  = Tip a
+  | Bin (Tree a) (Tree a)
+  deriving (Show, Eq, Functor, Generic, Generic1, Authable)
 
-instance Hashable a => Hashable (List a)
+toAuthTree :: Hashable a => Tree a -> AuthTree a
+toAuthTree (Bin l r) =
+    AuthBin (toHash (Bin l r)) (toAuthTree l) (toAuthTree r)
+toAuthTree (Tip a) =
+    AuthTip (toHash (Tip a)) a
+
+exampleTree :: Tree Int
+exampleTree = Bin (Bin (Tip 3) (Bin (Bin (Tip 2) (Tip 5)) (Tip 8))) (Tip 1)
+
+constructProof :: (Hashable a, Eq a) => AuthTree a -> a -> Proof
+constructProof = constructProof' []
+
+constructProof' :: (Hashable a, Eq a) => Proof -> AuthTree a -> a -> Proof
+constructProof' path (AuthTip _ a) item
+    | a == item = path
+    | otherwise = []
+constructProof' path (AuthBin _ l r) item = lpath ++ rpath
+    where
+        proofItem s n1@AuthBin{} n2@AuthBin{} =
+            ProofElem s (binHash l) (binHash r)
+        proofItem s n1@AuthBin{} n2@AuthTip{} =
+            ProofElem s (binHash l) (tipHash r)
+        proofItem s n1@AuthTip{} n2@AuthBin{} =
+            ProofElem s (tipHash l) (binHash r)
+        proofItem s n1@AuthTip{} n2@AuthTip{} =
+            ProofElem s (tipHash l) (tipHash r)
+        lpath = constructProof' (proofItem L l r : path) l item
+        rpath = constructProof' (proofItem R l r : path) r item
+
+instance (Hashable a) => Hashable (Tree a) where
+  toHash (Bin l r) = toHash (getHash (toHash l) <> getHash (toHash r))
+  toHash (Tip a) = toHash a
+
+
--- src/Auth.hs
+++ src/Auth.hs
@@ -14,7 +14,13 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeFamilies #-}
 
-module Auth where
+module Auth 
+    ( verify
+    , Authable(..)
+    , Proof
+    , ProofElem(..)
+    , Side(..)
+    ) where
 
 import Protolude hiding (show, Hashable(..))
 import Prelude (Show(..))
@@ -28,53 +34,7 @@ import qualified Data.ByteArray.Encoding as BA
 
 import GHC.Generics
 
-newtype Hash = Hash { rawHash :: Digest SHA3_256 }
-  deriving (Eq, Ord, Show, BA.ByteArrayAccess)
-
--- sha256 hash
-sha256 :: ByteString -> Hash
-sha256 x = Hash (hash x :: Digest SHA3_256)
-
--- | Size of SHA256 output.
-hashSize :: Int
-hashSize = 32
-
--- | All zero hash
-emptyHash :: Hash
-emptyHash = Hash digest
-  where
-    digest :: Digest SHA3_256
-    digest = unsafeFromJust $ digestFromByteString (BS.replicate hashSize 0)
-
--- | Use Haskell's Show representation (converted to ByteString) for the
--- contents of the SHA hash.
-showHash :: Show a => a -> ByteString
-showHash = toS . show
-
-getHash :: Hash -> ByteString
-getHash = BA.convert . rawHash
-
-data AuthTree a
-  = AuthTip { tipHash :: Hash, tipValue :: a }
-  | AuthBin { binHash :: Hash, lAuthTree :: (AuthTree a), rAuthTree :: (AuthTree a) }
-  deriving (Show, Functor, Generic)
-
-data Tree a
-  = Tip a
-  | Bin (Tree a) (Tree a)
-  deriving (Show, Eq, Functor, Generic, Generic1, Authable)
-
-toAuthTree :: Hashable a => Tree a -> AuthTree a
-toAuthTree (Bin l r) =
-    AuthBin (toHash (Bin l r)) (toAuthTree l) (toAuthTree r)
-toAuthTree (Tip a) =
-    AuthTip (toHash (Tip a)) a
-
-exampleTree :: Tree Int
-exampleTree = Bin (Bin (Tip 3) (Bin (Bin (Tip 2) (Tip 5)) (Tip 8))) (Tip 1)
-
-constructProof :: (Hashable a, Eq a) => AuthTree a -> a -> Proof
-constructProof = constructProof' []
+import Hash
 
 type Proof = [ProofElem]
 
@@ -86,27 +46,14 @@ data ProofElem = ProofElem
     , rightHash :: Hash
     } deriving (Show, Eq)
 
-constructProof' :: (Hashable a, Eq a) => Proof -> AuthTree a -> a -> Proof
-constructProof' path (AuthTip _ a) item
-    | a == item = path
-    | otherwise = []
-constructProof' path (AuthBin _ l r) item = lpath ++ rpath
-    where
-        proofItem s n1@AuthBin{} n2@AuthBin{} =
-            ProofElem s (binHash l) (binHash r)
-        proofItem s n1@AuthBin{} n2@AuthTip{} =
-            ProofElem s (binHash l) (tipHash r)
-        proofItem s n1@AuthTip{} n2@AuthBin{} =
-            ProofElem s (tipHash l) (binHash r)
-        proofItem s n1@AuthTip{} n2@AuthTip{} =
-            ProofElem s (tipHash l) (tipHash r)
-        lpath = constructProof' (proofItem L l r : path) l item
-        rpath = constructProof' (proofItem R l r : path) r item
-
-data AuthT a = AuthT Hash a | AuthTEmpty
-
-type Root = Hash
-
+-- | The verifier begins with the proof stream and
+-- just the hash of the authenticated data structure.
+-- It first compares this hash to the hash of the first
+-- element of the proof stream and checks its side.
+-- If L, the left hash of the first element will now become
+-- the parent hash that compares with the hash of the second element
+-- until it gets to the last element of the proof.
+-- The verifier then ...
 verify :: Hashable a => Hash -> Proof -> a -> Bool
 verify _ [] _ = False
 verify parentHash (proof:proofs) a
@@ -175,76 +122,5 @@ instance (GAuthable a, GAuthable b) => GAuthable (a :*: b) where
     gProveHash (a :*: b) = 
         toHash (getHash (gProveHash a) <> getHash (gProveHash b))
 
--------------------------------------------------------------------------------
--- Hashing
--------------------------------------------------------------------------------
-
-class Show a => Hashable a where
-  toHash :: a -> Hash
-  -- | SHA hash ( generic deriving )
-  default toHash :: (Generic a, GHashable' (Rep a)) => a -> Hash
-  toHash a = gtoHash (from a)
-
-  -- | Covert to string
-  toBS :: a -> ByteString
-  toBS a = (prefix a) <> (showHash a)
-
-  -- | Prefix the hash input with custom data to distinguish unique types
-  prefix :: a -> ByteString
-  prefix = const mempty
-
-class GHashable' f where
-  gtoHash :: f a -> Hash
-  gtoBS :: f a -> ByteString
-
-instance GHashable' U1 where
-  gtoHash _ = emptyHash
-  gtoBS _ = ""
-
-instance GHashable' (Rec1 f) where
-  gtoHash (Rec1 f) = emptyHash
-  gtoBS _ = ""
-
-instance (Hashable c) => GHashable' (K1 i c) where
-  gtoHash (K1 a) = toHash a
-  gtoBS (K1 a) = prefix a <> toBS a
-
-instance (GHashable' a) => GHashable' (M1 i c a) where
-  gtoHash (M1 a) = gtoHash a
-  gtoBS (M1 a) = gtoBS a
-
-instance (GHashable' a, GHashable' b) => GHashable' (a :+: b) where
-  gtoHash (L1 a) = gtoHash a
-  gtoHash (R1 a) = gtoHash a
-
-  gtoBS (L1 a) = gtoBS a
-  gtoBS (R1 a) = gtoBS a
-
-instance (GHashable' a, GHashable' b) => GHashable' (a :*: b) where
-  gtoHash (a :*: b) = toHash (gtoBS a <> gtoBS b)
-  gtoBS (a :*: b) = gtoBS a <> gtoBS b
-
--- Instances
-
-instance Hashable ByteString where
-  toHash = Hash . hash
-  toBS = identity
-
-instance Hashable Int where
-  toHash = Hash . hash . showHash
-
-instance Hashable Bool where
-  toHash = Hash . hash . showHash
-
-instance (Hashable a) => Hashable (Tree a) where
-  toHash (Bin l r) = toHash (getHash (toHash l) <> getHash (toHash r))
-  toHash (Tip a) = toHash a
-
 instance Hashable ProofElem where
   toHash (ProofElem s l r) = toHash (getHash l <> getHash r)
-
-instance (Hashable a, Hashable b) => Hashable (a,b)
-
-instance (Hashable a, Hashable b) => Hashable (Either a b) where
-  toHash (Left x) = toHash x
-  toHash (Right x) = toHash x
