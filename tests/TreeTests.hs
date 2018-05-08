5cafb1f7c233d6c1e498dddbe818898424ac016e
Alberto Centelles <centelles.alberto@gmail.com>
2018-05-08 17:53:16 +0100

    Add binTree tests

--- tests/TreeTests.hs
+++ tests/TreeTests.hs
@@ -20,15 +20,66 @@ data Tree a
   | Bin (Tree a) (Tree a)
   deriving (Show, Eq, Functor, Generic, Generic1, Authable, Hashable)
 
+exampleTreeInt :: Tree Int
+exampleTreeInt = Bin (Bin (Tip 3) (Bin (Bin (Tip 2) (Tip 5)) (Tip 8))) (Tip 1)
+
 instance Arbitrary a => Arbitrary (Tree a) where
   arbitrary = do
     m <- genPos
     arbitrarySizedTree m
 
+instance Arbitrary ByteString where
+    arbitrary = toS <$> (arbitrary :: Gen [Char]) `suchThat` (/= "") 
+
+testGenericProof :: (Hashable a, Eq a) => Tree a -> a -> Bool
+testGenericProof tree a = verify rootHash proof a
+    where
+        proof = prove tree a
+        rootHash = toHash tree
+
+testTreeProperty :: (Hashable a, Eq a) => a -> Tree a -> Property
+testTreeProperty a tree = monadicIO $ do
+    tree' <- liftIO $ replaceRandomElem a tree
+    pure $ testGenericProof tree' a
+
+treeTests :: TestTree
+treeTests = testGroup "Trees Tests"
+  [ testCase "Verify valid element in proof should return true" $
+        assertBool "1, 2, 3, 5, 8 should be in the proof" $ 
+            not $ elem False 
+                [ testGenericProof exampleTreeInt 3
+                , testGenericProof exampleTreeInt 5
+                , testGenericProof exampleTreeInt 2
+                , testGenericProof exampleTreeInt 1
+                , testGenericProof exampleTreeInt 8
+                ]
+  , testCase "Verify invalid proof should return false" $
+        assertBool "4, 7, 11, 18, 29 should not be in the proof" $
+            not $ elem True
+                [ testGenericProof exampleTreeInt 4
+                , testGenericProof exampleTreeInt 7
+                , testGenericProof exampleTreeInt 11
+                , testGenericProof exampleTreeInt 18
+                , testGenericProof exampleTreeInt 29
+                ]
+   , testProperty "Verify arbitrarily sized trees of ints" $
+        forAll arbitrary $ \t ->
+        forAll (genPosUnique t) $ \a -> testTreeProperty a t
+
+   ,  localOption (QuickCheckTests 10) $
+        testProperty "Verify arbitrarily sized trees of bytestrings" $
+            (testTreeProperty :: ByteString -> Tree ByteString -> Property)
+  ]
+
 genPosUnique :: Tree Int -> Gen Int
 genPosUnique t = do
     a <- abs <$> arbitrary `suchThat` (> 1)
     if findElem a t then genPosUnique t else pure a
+
+genBSUnique :: Tree ByteString -> Gen ByteString
+genBSUnique t = do
+    a <- arbitrary
+    if findElem a t then genBSUnique t else pure a
 
 genPos :: Gen Int
 genPos = abs <$> arbitrary `suchThat` (> 1)
@@ -41,10 +92,7 @@ arbitrarySizedTree m = do
     1 -> pure $ Tip a
     otherwise -> Bin <$> (arbitrarySizedTree n) <*> (arbitrarySizedTree (m - n))
 
-exampleTree :: Tree Int
-exampleTree = Bin (Bin (Tip 3) (Bin (Bin (Tip 2) (Tip 5)) (Tip 8))) (Tip 1)
-
-findElem :: Eq a => a -> Tree a -> Bool 
+findElem :: Eq a => a -> Tree a -> Bool
 findElem a (Tip b) = a == b
 findElem a (Bin l r) = findElem a l || findElem a r
 
@@ -52,42 +100,6 @@ replaceRandomElem :: Eq a => a -> Tree a -> IO (Tree a)
 replaceRandomElem a (Tip b) = pure $ Tip a
 replaceRandomElem a (Bin l r) = do
     b <- randomIO
-    if b 
+    if b
         then Bin <$> replaceRandomElem a l <*> pure r
         else Bin <$> pure l <*> replaceRandomElem a r
-
-testGenericProof :: Tree Int -> Int -> Bool
-testGenericProof tree a = verify rootHash proof a
-    where
-        proof = prove tree a
-        rootHash = toHash tree
-
-treeTests :: TestTree
-treeTests = testGroup "Authenticated Data Structures"
-  [ testCase "verify valid element in proof should return true" $
-        assertBool "1, 2, 3, 5, 8 should be in the proof" $ 
-            not $ elem False 
-                [ testGenericProof exampleTree 3
-                , testGenericProof exampleTree 5
-                , testGenericProof exampleTree 2
-                , testGenericProof exampleTree 1
-                , testGenericProof exampleTree 8
-                ]
-  , testCase "Verify invalid proof should return false" $
-        assertBool "4, 7, 11, 18, 29 should not be in the proof" $
-            not $ elem True
-                [ testGenericProof exampleTree 4
-                , testGenericProof exampleTree 7
-                , testGenericProof exampleTree 11
-                , testGenericProof exampleTree 18
-                , testGenericProof exampleTree 29
-                ]
-   , testProperty "Verify arbitrarily sized trees" $
-        forAll arbitrary $ \t ->
-        forAll (genPosUnique t) $ \a -> testTreeProperty a t
-  ]
-
-testTreeProperty :: Int -> Tree Int -> Property
-testTreeProperty a tree = monadicIO $ do 
-    tree' <- liftIO $ replaceRandomElem a tree
-    pure $ testGenericProof tree' a
